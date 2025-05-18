cad4e45cb3c3184b349c3f2077e225bba80adf20
x2w <x2w.soda@gmail.com>
2025-05-18 17:19:07 +0800

    update serialization tests

--- Core/Serial/Test/SerialTest.cpp
+++ Core/Serial/Test/SerialTest.cpp
@@ -1,77 +1,98 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include "Extra/doctest/doctest.h"
+#include <Ludens/Header/Math/Rect.h>
+#include <Ludens/Header/Math/Vec2.h>
 #include <Ludens/Serial/Serial.h>
+#include <string>
 
-namespace {
+using namespace LD;
 
-struct Vec2
+struct Foo
 {
-    float x, y;
+    std::string name;
+    Rect hitbox;
+    Rect hurtbox;
+    int32_t health;
 
-    static void serialize(LD::Serializer& serializer, const Vec2& foo)
+    static void serialize(Serializer& serial, const Foo& foo)
     {
-        serializer.write_f32(foo.x);
-        serializer.write_f32(foo.y);
+        serial.write_u32((uint32_t)foo.name.size());
+        serial.write((byte*)foo.name.data(), foo.name.size());
+        serial.write_i32(foo.health);
+        serial.write_f32(foo.hitbox.x);
+        serial.write_f32(foo.hitbox.y);
+        serial.write_f32(foo.hitbox.w);
+        serial.write_f32(foo.hitbox.h);
+        serial.write_f32(foo.hurtbox.x);
+        serial.write_f32(foo.hurtbox.y);
+        serial.write_f32(foo.hurtbox.w);
+        serial.write_f32(foo.hurtbox.h);
     }
 
-    static void deserialize(LD::Serializer& serializer, Vec2& foo)
+    static void deserialize(Serializer& serial, Foo& foo)
     {
-        serializer.read_f32(foo.x);
-        serializer.read_f32(foo.y);
+        uint32_t nameLen;
+        serial.read_u32(nameLen);
+        foo.name.resize(nameLen);
+        serial.read((byte*)foo.name.data(), nameLen);
+        serial.read_i32(foo.health);
+        serial.read_f32(foo.hitbox.x);
+        serial.read_f32(foo.hitbox.y);
+        serial.read_f32(foo.hitbox.w);
+        serial.read_f32(foo.hitbox.h);
+        serial.read_f32(foo.hurtbox.x);
+        serial.read_f32(foo.hurtbox.y);
+        serial.read_f32(foo.hurtbox.w);
+        serial.read_f32(foo.hurtbox.h);
     }
 };
 
-struct Rect
+TEST_CASE("Serializer API")
 {
-    Vec2 min;
-    Vec2 max;
+    Vec2 v2(2.0f, 3.0f);
+    Vec3 v3(v2, 4.0f);
+    Vec4 v4(v3, 5.0f);
 
-    static void serialize(LD::Serializer& serializer, const Rect& rect)
-    {
-        Vec2::serialize(serializer, rect.min);
-        Vec2::serialize(serializer, rect.max);
-    }
+    Serializer serial;
+    serial.write_f32(3.14f);
+    serial.write_vec2(v2);
+    serial.write_vec3(v3);
+    serial.write_vec4(v4);
+    CHECK(serial.size() == sizeof(float) * 10);
 
-    static void deserialize(LD::Serializer& serializer, Rect& rect)
-    {
-        Vec2::deserialize(serializer, rect.min);
-        Vec2::deserialize(serializer, rect.max);
-    }
-};
-
-} // namespace
+    float f;
+    serial.read_f32(f);
+    serial.read_vec2(v2);
+    serial.read_vec3(v3);
+    serial.read_vec4(v4);
+    CHECK(f == 3.14f);
+    CHECK(v2 == Vec2(2.0f, 3.0f));
+    CHECK(v3 == Vec3(2.0f, 3.0f, 4.0f));
+    CHECK(v4 == Vec4(2.0f, 3.0f, 4.0f, 5.0f));
+}
 
 TEST_CASE("struct serialization")
 {
-    Vec2 foo{2.0f, 3.0f};
+    Foo f;
+    f.name = "gameobject";
+    f.health = 100;
+    f.hitbox = {1.0f, 2.0f, 3.0f, 4.0f};
+    f.hurtbox = {5.0f, 6.0f, 7.0f, 8.0f};
 
-    LD::Serializer serial;
+    Serializer serial;
+    serialize(serial, f);
 
-    LD::serialize(serial, foo);
-    CHECK(serial.size() == 8);
+    Foo f2;
+    deserialize(serial, f2);
 
-    Vec2 restore;
-    LD::deserialize(serial, restore);
-    CHECK(restore.x == foo.x);
-    CHECK(restore.y == foo.y);
-}
-
-TEST_CASE("aggregate serialization")
-{
-    Rect rect;
-    rect.min = {2.0f, 3.0f};
-    rect.max = {8.0f, 24.0f};
-
-    LD::Serializer serial;
-
-    LD::serialize(serial, rect);
-    CHECK(serial.size() == 16);
-
-    Rect restore;
-    LD::deserialize(serial, restore);
-
-    CHECK(restore.min.x == rect.min.x);
-    CHECK(restore.min.y == rect.min.y);
-    CHECK(restore.max.x == rect.max.x);
-    CHECK(restore.max.y == rect.max.y);
+    CHECK(f2.name == "gameobject");
+    CHECK(f2.health == 100);
+    CHECK(f2.hitbox.x == f.hitbox.x);
+    CHECK(f2.hitbox.y == f.hitbox.y);
+    CHECK(f2.hitbox.w == f.hitbox.w);
+    CHECK(f2.hitbox.h == f.hitbox.h);
+    CHECK(f2.hurtbox.x == f.hurtbox.x);
+    CHECK(f2.hurtbox.y == f.hurtbox.y);
+    CHECK(f2.hurtbox.w == f.hurtbox.w);
+    CHECK(f2.hurtbox.h == f.hurtbox.h);
 }
