ef6cbb6deb625afbcf45e4a1b0287fed1fb142aa
x2w <x2w.soda@gmail.com>
2025-05-22 15:16:56 +0800

    font rendering
    support bitmap font rendering and single channel SDF font rendering.
    FontAtlas create_bitmap
    FontAtlas get_atlas_glyph
    FontAtlas get_baseline_glyph
    will add msdf later.

--- Core/Media/Lib/Font.cpp
+++ Core/Media/Lib/Font.cpp
@@ -1,9 +1,15 @@
+#include "GlyphTable.h"
 #include <Ludens/Header/Assert.h>
-#include <Ludens/Media/Font.h>
 #include <Ludens/Media/Bitmap.h>
+#include <Ludens/Media/Font.h>
+#include <Ludens/Profiler/Profiler.h>
 #include <Ludens/System/Memory.h>
+#include <algorithm>
 #include <msdf-atlas-gen/msdf-atlas-gen.h> // hide from user
 #include <vector>
+
+#define MAX_CORNER_ANGLE 3.0
+#define GENERATOR_THREAD_COUNT 4
 
 namespace LD {
 
@@ -16,15 +22,113 @@ struct FontObj
 
 struct FontAtlasObj
 {
+    FontAtlasType type;
     Font font;
     Bitmap atlas;
-    std::vector<msdf_atlas::GlyphGeometry> glyphs;
+    GlyphTable table;
+    float fontSize;
 };
 
-static Bitmap generate_sdf_atlas(FontObj* obj, std::vector<msdf_atlas::GlyphGeometry>& glyphs);
+struct FontAtlasConfig
+{
+    float fontSize;
+    float spacing;
+    float pixelRange;
+};
+
+static Bitmap generate_bitmap_atlas(FontObj* obj, std::vector<msdf_atlas::GlyphGeometry>& glyphs, const FontAtlasConfig& config);
+static Bitmap generate_sdf_atlas(FontObj* obj, std::vector<msdf_atlas::GlyphGeometry>& glyphs, const FontAtlasConfig& config);
+
+static Bitmap generate_bitmap_atlas(FontObj* obj, std::vector<msdf_atlas::GlyphGeometry>& glyphs, const FontAtlasConfig& config)
+{
+    LD_PROFILE_SCOPE;
+
+    msdfgen::FontHandle* font = obj->msdfHandle;
+
+    msdf_atlas::FontGeometry fontGeometry(&glyphs);
+    fontGeometry.loadCharset(font, 1.0f, msdf_atlas::Charset::ASCII);
+
+    for (msdf_atlas::GlyphGeometry& glyph : glyphs)
+        glyph.edgeColoring(&msdfgen::edgeColoringSimple, MAX_CORNER_ANGLE, 0);
+
+    msdf_atlas::GridAtlasPacker packer;
+    packer.setDimensionsConstraint(msdf_atlas::DimensionsConstraint::SQUARE);
+    packer.setMinimumScale(config.fontSize);
+    packer.setPixelRange(config.pixelRange);
+    packer.setMiterLimit(1.0);
+    packer.setSpacing(config.spacing);
+    packer.pack(glyphs.data(), (int)glyphs.size());
+
+    int atlasWidth, atlasHeight;
+    packer.getDimensions(atlasWidth, atlasHeight);
+    msdf_atlas::ImmediateAtlasGenerator<
+        float, 1,
+        msdf_atlas::scanlineGenerator,
+        msdf_atlas::BitmapAtlasStorage<msdfgen::byte, 1>>
+        generator(atlasWidth, atlasHeight);
+
+    msdf_atlas::GeneratorAttributes attr{};
+    attr.scanlinePass = true;
+    attr.config.overlapSupport = true;
+
+    generator.setAttributes(attr);
+    generator.setThreadCount(GENERATOR_THREAD_COUNT);
+    generator.generate(glyphs.data(), (int)glyphs.size());
+
+    const msdfgen::BitmapConstRef<msdfgen::byte, 1>& bitmap = generator.atlasStorage();
+    Bitmap handle = Bitmap::create_from_data(bitmap.width, bitmap.height, BITMAP_CHANNEL_R, bitmap.pixels);
+    handle.flipy();
+
+    return handle;
+}
+
+static Bitmap generate_sdf_atlas(FontObj* obj, std::vector<msdf_atlas::GlyphGeometry>& glyphs, const FontAtlasConfig& config)
+{
+    LD_PROFILE_SCOPE;
+
+    msdfgen::FontHandle* font = obj->msdfHandle;
+
+    msdf_atlas::FontGeometry fontGeometry(&glyphs);
+    fontGeometry.loadCharset(font, 1.0f, msdf_atlas::Charset::ASCII);
+
+    for (msdf_atlas::GlyphGeometry& glyph : glyphs)
+        glyph.edgeColoring(&msdfgen::edgeColoringSimple, MAX_CORNER_ANGLE, 0);
+
+    msdf_atlas::GridAtlasPacker packer;
+    packer.setDimensionsConstraint(msdf_atlas::DimensionsConstraint::SQUARE);
+    packer.setMinimumScale(config.fontSize);
+    packer.setPixelRange(config.pixelRange);
+    packer.setMiterLimit(1.0);
+    packer.setSpacing(config.spacing);
+    packer.pack(glyphs.data(), (int)glyphs.size());
+
+    int atlasWidth, atlasHeight;
+    packer.getDimensions(atlasWidth, atlasHeight);
+    msdf_atlas::ImmediateAtlasGenerator<
+        float, 1,
+        msdf_atlas::sdfGenerator,
+        msdf_atlas::BitmapAtlasStorage<msdfgen::byte, 1>>
+        generator(atlasWidth, atlasHeight);
+
+    msdf_atlas::GeneratorAttributes attr{};
+    attr.scanlinePass = true;
+    attr.config.overlapSupport = true;
+
+    generator.setAttributes(attr);
+    generator.setThreadCount(GENERATOR_THREAD_COUNT);
+    generator.generate(glyphs.data(), (int)glyphs.size());
+
+    const msdfgen::BitmapConstRef<msdfgen::byte, 1>& bitmap = generator.atlasStorage();
+    Bitmap handle = Bitmap::create_from_data(bitmap.width, bitmap.height, BITMAP_CHANNEL_R, bitmap.pixels);
+    handle.flipy();
+
+    return handle;
+}
 
 Font Font::create_from_path(const char* path)
 {
+    LD_PROFILE_SCOPE;
+
     FontObj* obj = heap_new<FontObj>(MEMORY_USAGE_MEDIA);
 
     if (!msdfFreeType)
@@ -39,8 +143,20 @@ Font Font::create_from_path(const char* path)
     return {obj};
 }
 
+void Font::get_metrics(FontMetrics& metrics, float fontSizePx)
+{
+    msdfgen::FontMetrics msdfMetrics;
+    msdfgen::getFontMetrics(msdfMetrics, mObj->msdfHandle, msdfgen::FONT_SCALING_EM_NORMALIZED);
+
+    metrics.ascent = msdfMetrics.ascenderY * fontSizePx;
+    metrics.descent = msdfMetrics.descenderY * fontSizePx;
+    metrics.lineHeight = msdfMetrics.lineHeight * fontSizePx;
+}
+
 void Font::destroy(Font font)
 {
+    LD_PROFILE_SCOPE;
+
     FontObj* obj = (FontObj*)font;
 
     msdfgen::destroyFont(obj->msdfHandle);
@@ -48,55 +164,52 @@ void Font::destroy(Font font)
     heap_delete<FontObj>(obj);
 }
 
-static Bitmap generate_sdf_atlas(FontObj* obj, std::vector<msdf_atlas::GlyphGeometry>& glyphs)
+FontAtlas FontAtlas::create_bitmap(Font font, float fontSize)
 {
-    msdfgen::FontHandle* font = obj->msdfHandle;
+    LD_PROFILE_SCOPE;
 
-    msdf_atlas::FontGeometry fontGeometry(&glyphs);
-    fontGeometry.loadCharset(font, 1.0f, msdf_atlas::Charset::ASCII);
+    FontAtlasObj* obj = heap_new<FontAtlasObj>(MEMORY_USAGE_MEDIA);
+    obj->type = FONT_ATLAS_BITMAP;
+    obj->font = font;
+    obj->fontSize = fontSize;
 
-    const double maxCornerAngle = 3.0f;
-    for (msdf_atlas::GlyphGeometry& glyph : glyphs)
-        glyph.edgeColoring(&msdfgen::edgeColoringInkTrap, maxCornerAngle, 0);
+    std::vector<msdf_atlas::GlyphGeometry> glyphs;
 
-    msdf_atlas::GridAtlasPacker packer;
+    FontAtlasConfig config;
+    config.fontSize = fontSize;
+    config.pixelRange = 1.0f;
+    config.spacing = 0.0f;
+    obj->atlas = generate_bitmap_atlas(font, glyphs, config);
+    obj->table.build(glyphs, obj->atlas.width(), obj->atlas.height());
 
-    packer.setDimensionsConstraint(msdf_atlas::DimensionsConstraint::SQUARE);
-    packer.setMinimumScale(36.0);
-    packer.setPixelRange(2.0);
-    packer.setMiterLimit(1.0);
-    packer.setSpacing(4);
-    packer.pack(glyphs.data(), (int)glyphs.size());
-
-    int atlasWidth, atlasHeight;
-    packer.getDimensions(atlasWidth, atlasHeight);
-    msdf_atlas::ImmediateAtlasGenerator<
-        float, 1,                 // number of atlas color channels
-        msdf_atlas::sdfGenerator, // function to generate bitmaps for individual glyphs
-        msdf_atlas::BitmapAtlasStorage<msdfgen::byte, 1>>
-        generator(atlasWidth, atlasHeight);
-
-    generator.setAttributes({});
-    generator.setThreadCount(4);
-    generator.generate(glyphs.data(), (int)glyphs.size());
-
-    const msdfgen::BitmapConstRef<msdfgen::byte, 1>& bitmap = generator.atlasStorage();
-    return Bitmap::create_from_data(bitmap.width, bitmap.height, BITMAP_CHANNEL_R, bitmap.pixels);
+    return {obj};
 }
 
-FontAtlas FontAtlas::create_sdf(Font font)
+FontAtlas FontAtlas::create_sdf(Font font, float fontSize)
 {
-    FontAtlasObj* obj = heap_new<FontAtlasObj>(MEMORY_USAGE_MEDIA);
+    LD_PROFILE_SCOPE;
 
-    // TODO: configuration
+    FontAtlasObj* obj = heap_new<FontAtlasObj>(MEMORY_USAGE_MEDIA);
+    obj->type = FONT_ATLAS_SDF;
     obj->font = font;
-    obj->atlas = generate_sdf_atlas(font, obj->glyphs);
+    obj->fontSize = fontSize;
+
+    std::vector<msdf_atlas::GlyphGeometry> glyphs;
+
+    FontAtlasConfig config;
+    config.fontSize = fontSize;
+    config.pixelRange = 2.0f;
+    config.spacing = 0.0f;
+    obj->atlas = generate_sdf_atlas(font, glyphs, config);
+    obj->table.build(glyphs, obj->atlas.width(), obj->atlas.height());
 
     return {obj};
 }
 
 void FontAtlas::destroy(FontAtlas atlas)
 {
+    LD_PROFILE_SCOPE;
+
     FontAtlasObj* obj = (FontAtlasObj*)atlas;
 
     if (obj->atlas)
@@ -105,20 +218,56 @@ void FontAtlas::destroy(FontAtlas atlas)
     heap_delete<FontAtlasObj>(obj);
 }
 
+FontAtlasType FontAtlas::type()
+{
+    return mObj->type;
+}
+
+Font FontAtlas::get_font()
+{
+    return mObj->font;
+}
+
+float FontAtlas::get_font_size()
+{
+    return mObj->fontSize;
+}
+
+float FontAtlas::get_filter_ratio(float renderSize)
+{
+    return renderSize / mObj->fontSize;
+}
+
 Bitmap FontAtlas::get_bitmap()
 {
     return mObj->atlas;
 }
 
-bool FontAtlas::get_glyph(uint32_t code, int& x, int& y, int& w, int& h)
+bool FontAtlas::get_atlas_glyph(uint32_t code, IRect& glyphBB)
 {
-    if (code > (uint32_t)mObj->glyphs.size())
+    GlyphData glyph;
+
+    if (!mObj->table.find(code, glyph))
         return false;
 
-    LD_ASSERT(code < 128);
+    glyphBB = glyph.atlasBB;
+    return true;
+}
 
-    const msdf_atlas::GlyphGeometry* geo = mObj->glyphs.data() + (code - 32);
-    geo->getBoxRect(x, y, w, h);
+bool FontAtlas::get_baseline_glyph(uint32_t code, float fontSize, const Vec2& baseline, Rect& glyphBB, float& advanceX)
+{
+    GlyphData glyph;
+
+    if (!mObj->table.find(code, glyph))
+        return false;
+
+    float minx = baseline.x + glyph.baselineL * fontSize;
+    float maxx = baseline.x + glyph.baselineR * fontSize;
+    float miny = baseline.y + glyph.baselineT * fontSize;
+    float maxy = baseline.y + glyph.baselineB * fontSize;
+    glyphBB = Rect(minx, miny, maxx - minx, maxy - miny);
+
+    advanceX = glyph.advanceX * fontSize;
 
     return true;
 }
--- Include/Ludens/Media/Font.h
+++ Include/Ludens/Media/Font.h
@@ -1,28 +1,74 @@
 #pragma once
 
 #include <Ludens/Header/Handle.h>
+#include <Ludens/Header/Math/Rect.h>
+#include <Ludens/Header/Math/Vec2.h>
 #include <Ludens/Media/Bitmap.h>
 
 namespace LD {
+
+struct FontMetrics
+{
+    double ascent;
+    double descent;
+    double lineHeight;
+};
 
 struct Font : Handle<struct FontObj>
 {
     static Font create_from_path(const char* path);
     static void destroy(Font font);
+
+    void get_metrics(FontMetrics& metrics, float fontSizePx);
+};
+
+enum FontAtlasType
+{
+    FONT_ATLAS_BITMAP = 0,
+    FONT_ATLAS_SDF,
 };
 
 struct FontAtlas : Handle<struct FontAtlasObj>
 {
-    /// @brief create single channel signed distance field atlas for font
-    static FontAtlas create_sdf(Font font);
+    /// @brief create rasterized bitmap atlas for given font size
+    static FontAtlas create_bitmap(Font font, float fontSize);
 
+    /// @brief create single channel signed distance field atlas for font
+    static FontAtlas create_sdf(Font font, float fontSize);
+
+    /// @brief destroy font atlas
     static void destroy(FontAtlas atlas);
 
+    /// @brief get font atlas type
+    FontAtlasType type();
+
+    /// @brief get the source font this atlas is generated with
+    Font get_font();
+
+    /// @brief get the font size this atlas is generated with
+    float get_font_size();
+
+    /// @brief The filter ratio is the ratio of the rendered pixel size to the actual bitmap pixel size.
+    ///        If this ratio is less than one, we have minification filtering. If this ratio is greater
+    ///        than one, we have magnification filtering.
+    /// @info unless using SDF-based bitmap, try to keep this ratio close to one.
+    float get_filter_ratio(float renderSize);
+
+    /// @brief get atlas bitmap
     Bitmap get_bitmap();
 
     /// @brief get glyph bounding box in atlas
-    /// @param code unicode codepoint 
-    bool get_glyph(uint32_t code, int& x, int& y, int& w, int& h);
+    /// @param code unicode codepoint
+    /// @param glyphBB glyph BB in atlas, pixel positions
+    bool get_atlas_glyph(uint32_t code, IRect& glyphBB);
+
+    /// @brief get glyph bounding box in screen space, relative to baseline position
+    /// @param code unicode codepoint
+    /// @param fontSize rendering size in screen space
+    /// @param baseline baseline cursor position in screen space
+    /// @param glyphBB glyph BB in screen space
+    /// @param advanceX baseline cursor horizontal offset in screen space
+    bool get_baseline_glyph(uint32_t code, float fontSize, const Vec2& baseline, Rect& glyphBB, float& advanceX);
 };
 
 } // namespace LD
