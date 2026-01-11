098077f71624ce420eaf50be84b0fc83ed85cb05
x2w <x2w.soda@gmail.com>
2026-01-11 11:49:32 +0800

    refactor EditorMain

---
+++ LDEditor/EditorMain/Lib/EditorApplication.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <Ludens/AudioServer/AudioServer.h>
+#include <Ludens/Media/Font.h>
+#include <Ludens/RenderServer/RenderServer.h>
+#include <LudensEditor/EditorContext/EditorContext.h>
+#include <LudensEditor/EditorUI/EditorUI.h>
+
+namespace LD {
+
+class EditorApplication
+{
+public:
+    EditorApplication();
+    EditorApplication(const EditorApplication&) = delete;
+    EditorApplication(EditorApplication&&) = delete;
+    ~EditorApplication();
+
+    EditorApplication& operator=(const EditorApplication&) = delete;
+    EditorApplication& operator=(EditorApplication&&) = delete;
+
+    void run();
+
+private:
+    RDevice mRDevice;
+    RenderServer mRenderServer;
+    AudioServer mAudioServer;
+    EditorContext mEditorCtx;
+    EditorUI mEditorUI;
+    Font mFont;
+    FontAtlas mFontAtlas;
+    RImage mFontAtlasImage;
+    RUID mEnvCubemap;
+};
+
+} // namespace LD
