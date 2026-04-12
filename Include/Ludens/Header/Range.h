05a36f6d7d28d4f2821454d07af1e96a55fab6df
x2w <x2w.soda@gmail.com>
2026-04-12 16:39:02 -0500

    Ranges stuff

--- Include/Ludens/Header/Range.h
+++ Include/Ludens/Header/Range.h
@@ -10,6 +10,34 @@ struct TRange
     T offset;
     T size;
 
+    /// @brief Construct range from two endpoint offsets.
+    static TRange from_offsets(T offset1, T offset2)
+    {
+        if (offset1 > offset2)
+        {
+            T tmp = offset1;
+            offset1 = offset2;
+            offset2 = tmp;
+        }
+
+        return TRange(offset1, offset2 - offset1);
+    }
+
+    /// @brief Construct range by clamping against some size.
+    static TRange clamp_size(TRange range, size_t clampSize)
+    {
+        if (clampSize == 0)
+            return TRange((T)0, (T)0);
+
+        if (range.offset >= clampSize)
+            return TRange(clampSize, (T)0);
+
+        if (range.offset + range.size >= clampSize)
+            return TRange(range.offset, clampSize - range.offset);
+
+        return range;
+    }
+
     inline operator bool() const noexcept
     {
         return size > 0;
--- LDCore/Header/Test/RangeTest.cpp
+++ LDCore/Header/Test/RangeTest.cpp
@@ -1,6 +1,54 @@
+#include <Extra/doctest/doctest.h>
 #include <Ludens/Header/Range.h>
 #include <type_traits>
 
 using namespace LD;
 
 static_assert(std::is_trivial_v<Range>);
+
+TEST_CASE("Range from_offsets")
+{
+    Range range;
+    range = Range::from_offsets(0, 0);
+    CHECK(range == Range(0, 0));
+
+    range = Range::from_offsets(0, 4);
+    CHECK(range == Range(0, 4));
+
+    range = Range::from_offsets(4, 0);
+    CHECK(range == Range(0, 4));
+
+    range = Range::from_offsets(4, 9);
+    CHECK(range == Range(4, 5));
+
+    range = Range::from_offsets(9, 4);
+    CHECK(range == Range(4, 5));
+}
+
+TEST_CASE("Range clamp_size")
+{
+    Range range;
+    range = Range::clamp_size(Range(0, 0), 0);
+    CHECK(range == Range(0, 0));
+
+    range = Range::clamp_size(Range(4, 0), 0);
+    CHECK(range == Range(0, 0));
+
+    range = Range::clamp_size(Range(4, 0), 3);
+    CHECK(range == Range(3, 0)); // clamp offset
+
+    range = Range::clamp_size(Range(4, 5), 3);
+    CHECK(range == Range(3, 0));
+
+    range = Range::clamp_size(Range(4, 5), 9);
+    CHECK(range == Range(4, 5));
+
+    range = Range::clamp_size(Range(4, 5), 8);
+    CHECK(range == Range(4, 4));
+
+    range = Range::clamp_size(Range(4, 5), 5);
+    CHECK(range == Range(4, 1));
+
+    range = Range::clamp_size(Range(4, 5), 4);
+    CHECK(range == Range(4, 0));
+}
